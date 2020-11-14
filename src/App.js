7537af3eb1ad3b4e2d6c78ab400f5963b2b96e80
Jon Peach <jonmakesitbetter@gmail.com>
2020-11-14 18:18:22 -0500

    Test with image fix

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
   
       <Route exact path="/" component={Home} />
       <Route exact path="/contact" component={Contact} />
-      <Route path="/portfolio" component={Portfolio} />
+      <Route exact path="/portfolio" component={Portfolio} />
       </div>
       <Footer/>
     </Router>
