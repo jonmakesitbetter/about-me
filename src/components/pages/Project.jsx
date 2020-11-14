4787fad28448955afc7733bb99f50e3e5d8b8f34
Jon Peach <jonmakesitbetter@gmail.com>
2020-11-14 17:58:54 -0500

    Fixed homepage in package.json for troubleshooting

--- src/components/pages/Project.jsx
+++ src/components/pages/Project.jsx
@@ -10,7 +10,8 @@ const Project = (props) => {
           <div className="card project-card">
             <div className="img-container">
               <a href={props.siteLink}><strong>Project title: {props.name}</strong></a>
-              <img class="project-image" alt={props.name} src={props.image} />
+              {/* <img class="project-image" alt={props.name} src={props.image} /> */}
+              <img src={process.env.PUBLIC_URL + `${props.image}`} alt={props.name} />
             </div>
             <div className="content">
                   <a href={props.github}><strong>{props.name}'s Github repository</strong></a>
